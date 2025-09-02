017b5b4b5cde396795c7848a769bc9cf91b88579
Tejaswita Shrivastava <tejaswita@esenseit.in>
2025-09-02 13:39:59 +0530

    first commit1

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const basePath = '/my-next-app';
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-   env: {
-    NEXT_PUBLIC_BASE_PATH: basePath, 
-  },
-  // output: 'export',
-  basePath: basePath, // must start with slash
   trailingSlash: true,
 };
 
